2942728c6022d9fe6f52dca821db6fff2f122026
Rakesh Patole <patolerakesh@gmail.com>
2021-07-25 13:43:44 +0530

    updated package description

--- Package.swift
+++ Package.swift
@@ -5,24 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "FluxStore",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "FluxStore",
-            targets: ["FluxStore"]),
+        .library(name: "FluxStore", targets: ["FluxStore"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "FluxStore",
-            dependencies: []),
-        .testTarget(
-            name: "FluxStoreTests",
-            dependencies: ["FluxStore"]),
+        .target(name: "FluxStore", dependencies: [])
     ]
 )
